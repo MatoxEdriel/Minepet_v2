570887a72ccc786d44f34fda84151e7c3259a218
MatoxEdriel <ga99_buggi@hotmail.com>
2026-01-22 23:08:59 -0500

    Ajustes de diseño y colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,13 @@ module.exports = {
             colors: {
                 'techfix-bg': '#F3F8FA',
                 'techfix-primary': '#0F1F2F',
-                'techfix-blue': '#4FA3D1',
+                'techfix-blue': '#140B05',
                 'techfix-orange': '#F39C4A',
+                'minepet-magenta': '#d946ef',
+
+
+
+
             },
         },
     },
